04386395385e01013e181793757cd8c6fe94e23e
yjh0410 <1394571815@qq.com>
2022-11-01 09:13:06 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # OpenVINO-Python-FreeYOLO
+
+## Requirements
+- We recommend you to use Anaconda to create a conda environment:
+```Shell
+conda create -n openvino_yolo python=3.6
+```
+
+- Then, activate the environment:
+```Shell
+conda activate openvino_yolo
+```
+
+- Requirements:
+```Shell
+pip install -r requirements.txt 
+```
+
+## Download FreeYOLO ONNX file
+Main results on COCO-val:
+
+| Model          |  Scale  |    AP    |    AP50    |  ONNX  |  XML  |
+|----------------|---------|----------|------------|--------|-------|
+| FreeYOLO-Nano  |  416    |      |        |  |  |
+| FreeYOLO-Tiny  |  416    |      |        |  |  |
+| FreeYOLO-Large |  640    |   47.1   |   67.4     | [github](https://github.com/yjh0410/FreeYOLO/releases/download/weight/yolo_free_large_opset_11.onnx) |  [github](https://github.com/yjh0410/FreeYOLO/releases/download/weight/yolo_free_large.xml) |
+| FreeYOLO-Huge  |  640    |     |        |  |  |
+
+
+## ONNXRuntime Demo
+
+```shell
+python3 openvino_inference.py --weight weights/xml/yolo_free_large.xml -i test_image.jpg -s 0.3 -size 640
+```
+or
+```shell
+python3 openvino_inference.py --weight weights/onnx/yolo_free_large_opset_11.onnx -i test_image.jpg -s 0.3 -size 640
+```
+
+Notes:
+* --weight: your converted onnx model or xml model
+* -i: input_image
+* -s: score threshold for visualization.
+* --img_size: should be consistent with the shape you used for onnx convertion.
---
+++ utils/vis_tools.py
@@ -0,0 +1,48 @@
+import cv2
+
+coco_class_labels = ('background',
+                        'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+                        'boat', 'traffic light', 'fire hydrant', 'street sign', 'stop sign',
+                        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+                        'elephant', 'bear', 'zebra', 'giraffe', 'hat', 'backpack', 'umbrella',
+                        'shoe', 'eye glasses', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+                        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+                        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'plate', 'wine glass',
+                        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+                        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+                        'couch', 'potted plant', 'bed', 'mirror', 'dining table', 'window', 'desk',
+                        'toilet', 'door', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+                        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'blender', 'book',
+                        'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+
+coco_class_index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20,
+                    21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+                    46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 67,
+                    70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+
+
+def plot_bbox_labels(img, bbox, label, cls_color, test_scale=0.4):
+    x1, y1, x2, y2 = bbox
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+    # plot bbox
+    cv2.rectangle(img, (x1, y1), (x2, y2), cls_color, 2)
+    # plot title bbox
+    cv2.rectangle(img, (x1, y1-t_size[1]), (int(x1 + t_size[0] * test_scale), y1), cls_color, -1)
+    # put the test on the title bbox
+    cv2.putText(img, label, (int(x1), int(y1 - 5)), 0, test_scale, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+
+    return img
+
+
+def visualize(img, bboxes, scores, labels, class_colors, vis_thresh=0.3):
+    ts = 0.4
+    for i, bbox in enumerate(bboxes):
+        if scores[i] > vis_thresh:
+            cls_color = class_colors[int(labels[i])]
+            cls_id = coco_class_index[int(labels[i])]
+            mess = '%s: %.2f' % (coco_class_labels[cls_id], scores[i])
+            img = plot_bbox_labels(img, bbox, mess, cls_color, test_scale=ts)
+
+    return img
+
